bb5f169f63f75c34029ab56ed798aae00f1daabb
jasontuan <doananhtuan22111996.com>
2018-06-30 20:30:51 +0700

    create project'

---
+++ app/src/main/java/com/tuan/coffeemanager/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.tuan.coffeemanager.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tuan.coffeemanager.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/tuan/coffeemanager/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.tuan.coffeemanager.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tuan.coffeemanager.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
